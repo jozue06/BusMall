8009e9cb5d0cc85c05bd1930a6cd11c7651f1f70
Jozue06 <jozue06@gmail.com>
2018-02-10 17:03:06 -0800

    trying to get total clicks to stop app.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 'use strict';
-//array to store the objects
 var allItems = [];
-var itemsShown = [];
 var globalClicks = 0;
-
-
-
-//constructor function
-function Item(name, filepath) {
-  this.name = name;
-  this.filepath = filepath;
-  this.timesClicked = 0;
+var img1;
+var img2;
+var img3;
+// var imageSection = document.getElementById('hide');
+// var resultsButton = document.getElementById('resultsButton');
+function Item(productName, filePath) {
+  this.productName = productName;
+  this.filePath = filePath;
+  this.clickTotal = 0;
+  this.timesDisplayed = 0;
+  this.percentClick = 0;
   allItems.push(this);
 }
 
-//constructor function variables
 new Item('Bag','img/bag.jpg');
 new Item('Banana', 'img/banana.jpg');
 new Item('Bathroom', 'img/bathroom.jpg');
@@ -33,49 +33,56 @@ new Item('USB', 'img/usb.gif');
 new Item('Water-can', 'img/water-can.jpg');
 new Item('Wine-glass', 'img/Wine-glass.jpg');
 
-//event listeners
-var imgEl1 = document.getElementById('pic1');
-var imgEl2 = document.getElementById('pic2');
-var imgEl3 = document.getElementById('pic3');
 
-imgEl1.addEventListener('click', onClick);
-imgEl2.addEventListener('click', onClick);
-imgEl3.addEventListener('click', onClick);
-
-// onclick fire multiple functions
-function onClick() {
-  for (var i = 0; i < 3; i++){
-    itemsShown[i].timesClicked++;
-    // console.log(itemsShown);
-  }
-  randomItem();
+if (globalClicks < 5) {
+  itemRender();
 }
 
-//randomly display three pictures, check if any of the pictures are duplicate, log global click totals.
-function randomItem() {
+var imgRandom = function () {
+  return Math.floor(Math.random() * allItems.length);
+};
+//function that makes images appear
+var itemRender = function(){
+  var itemPic1 = document.getElementById('pic1');
+  var itemPic2 = document.getElementById('pic2');
+  var itemPic3 = document.getElementById('pic3');
+  img1 = imgRandom();
+  itemPic1.src = allItems[img1].filePath;
+  allItems[img1].timesDisplayed ++;
+  img2 = imgRandom();
+  if (img1 === img2) {
+    img2 = imgRandom();
+  }
+  itemPic2.src = allItems[img2].filePath;
+  allItems[img2].timesDisplayed ++;
+  img3 = imgRandom();
+  if (img1 === img2 || img2 === img3 || img1 === img3) {
+    img3 = imgRandom();
+  }
+  itemPic3.src = allItems[img3].filePath;
+  allItems[img3].timesDisplayed ++;
+};
+itemRender();
+
+var itemPic1 = document.getElementById('pic1');
+var itemPic2 = document.getElementById('pic2');
+var itemPic3 = document.getElementById('pic3');
+
+
+
+function handleClick(image){
+  image.clickTotal++;
   globalClicks++;
-  console.log(globalClicks);
-  var randomIndex1 = Math.floor(Math.random() * allItems.length);
-  var randomIndex2 = Math.floor(Math.random() * allItems.length);
-  var randomIndex3 = Math.floor(Math.random() * allItems.length);
-  imgEl1.src = allItems[randomIndex1].filepath;
-  imgEl2.src = allItems[randomIndex2].filepath;
-  imgEl3.src = allItems[randomIndex3].filepath;
-  itemsShown[0]= allItems[randomIndex1];
-  itemsShown[1]= allItems[randomIndex2];
-  itemsShown[2]= allItems[randomIndex3];
-
-  if (randomIndex1 === randomIndex2 || randomIndex1 === randomIndex3 || randomIndex2 === randomIndex3){
-    globalClicks--;
-    randomItem();
-  }
+  console.log('total clicks ' + globalClicks);
+  console.log(image.productName + ' has ' + image.clickTotal + ' total votes.');
+  itemRender();
 }
-randomItem();
-
-var itemClicksTotals = document.getElementById('Totals');
-
-for(var i = 0; i < allItems.length; i++){
-  // console.log(itemsShown[i].name + allItems[i].timesClicked);
-  itemClicksTotals.textContent = (allItems[i].name + allItems[i].timesClicked);
-}
-
+itemPic1.addEventListener('click', function(){
+  handleClick(allItems[img1]);
+});
+itemPic2.addEventListener('click', function(){
+  handleClick(allItems[img2]);
+});
+itemPic3.addEventListener('click', function(){
+  handleClick(allItems[img3]);
+});
