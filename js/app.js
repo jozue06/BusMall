1987e0abc33eb65f5e10a896771cfa922fae65f9
Jozue06 <jozue06@gmail.com>
2018-02-11 14:14:21 -0800

    counter working

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ var globalClicks = 0;
 var img1;
 var img2;
 var img3;
+var itemDisplayed = [];
 // var imageSection = document.getElementById('hide');
 // var resultsButton = document.getElementById('resultsButton');
 function Item(productName, filePath) {
@@ -42,18 +43,26 @@ var itemRender = function(){
   var itemPic2 = document.getElementById('pic2');
   var itemPic3 = document.getElementById('pic3');
 
+  if (globalClicks === 25) {
+    removeDummy();
+  }
+  if(itemPic1 === itemPic2 || itemPic2 === itemPic3 || itemPic1 === itemPic3
+    || itemDisplayed.includes(itemPic1) || itemDisplayed.includes(itemPic2)
+    || itemDisplayed.includes(itemPic3)){
+    imgRandom();
+  }
   img1 = imgRandom();
   itemPic1.src = allItems[img1].filePath;
   allItems[img1].timesDisplayed ++;
   img2 = imgRandom();
   if (img1 === img2) {
-    img2 = imgRandom();
+    imgRandom();
   }
   itemPic2.src = allItems[img2].filePath;
   allItems[img2].timesDisplayed ++;
   img3 = imgRandom();
   if (img1 === img2 || img2 === img3 || img1 === img3) {
-    img3 = imgRandom();
+    imgRandom();
   }
   itemPic3.src = allItems[img3].filePath;
   allItems[img3].timesDisplayed ++;
@@ -64,21 +73,15 @@ var itemPic1 = document.getElementById('pic1');
 var itemPic2 = document.getElementById('pic2');
 var itemPic3 = document.getElementById('pic3');
 
-
 function handleClick(image){
   image.clickTotal++;
-  if (globalClicks < 25) {
-    globalClicks++;
-  }
-  else if (globalClicks === 25) {
-    itemPic1.removeEventListener('click', handleClick),
-    itemPic2.removeEventListener('click', handleClick),
-    itemPic3.removeEventListener('click', handleClick);
-  }
+  globalClicks++;
   console.log('total clicks ' + globalClicks);
   console.log(image.productName + ' has ' + image.clickTotal + ' total votes.');
+  itemDisplayed.push(itemPic1, itemPic2, itemPic3);
   itemRender();
 }
+
 itemPic1.addEventListener('click', function(){
   handleClick(allItems[img1]);
 });
@@ -89,3 +92,12 @@ itemPic3.addEventListener('click', function(){
   handleClick(allItems[img3]);
 });
 
+function removeDummy() {
+  var itemPic1 = document.getElementById('pic1');
+  var itemPic2 = document.getElementById('pic2');
+  var itemPic3 = document.getElementById('pic3');
+  var container = document.getElementById('container');
+  container.removeChild(itemPic1);
+  container.removeChild(itemPic2);
+  container.removeChild(itemPic3);
+}
