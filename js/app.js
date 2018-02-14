4ba9931e171ccb890305b7285a4a619c0f410f72
Jozue06 <jozue06@gmail.com>
2018-02-13 21:30:22 -0800

    refactored everything to get the rotated images not to duplicate

--- js/app.js
+++ js/app.js
@@ -1,100 +1,79 @@
 'use strict';
 
-var allItems = [];
-var globalClicks = 0;
-var img1;
-var img2;
-var img3;
-var itemDisplayed = [];
-var itemPic1 = document.getElementById('pic1');
-var itemPic2 = document.getElementById('pic2');
-var itemPic3 = document.getElementById('pic3');
-// var imageSection = document.getElementById('hide');
-// var resultsButton = document.getElementById('resultsButton');
-function Item(productName, filePath) {
-  this.productName = productName;
-  this.filePath = filePath;
-  this.clickTotal = 0;
-  this.timesDisplayed = 0;
-  this.percentClick = 0;
-  allItems.push(this);
+Item.names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
+Item.all = [];
+Item.container = document.getElementById('container');
+Item.viewed = [];
+
+Item.pics = [document.getElementById('pic1'),
+  document.getElementById('pic2'),
+  document.getElementById('pic3')];
+
+Item.totals = document.getElementById('totals');
+Item.totalClicks = 0;
+
+function Item(name) {
+  this.name = name;
+  this.path = 'img/' + name + '.jpg';
+  this.votes = 0;
+  this.views = 0;
+  Item.all.push(this);
+}
+for(var i = 0; i < Item.names.length; i++) {
+  new Item(Item.names[i]);
+}
+function makeRandom() {
+  return Math.floor(Math.random() * Item.names.length);
 }
 
-new Item('Bag','img/bag.jpg');
-new Item('Banana', 'img/banana.jpg');
-new Item('Bathroom', 'img/bathroom.jpg');
-new Item('Boots', 'img/boots.jpg');
-new Item('Breakfast', 'img/breakfast.jpg');
-new Item('Bubblegum', 'img/bubblegum.jpg');
-new Item('Chair', 'img/chair.jpg');
-new Item('Cthulhu', 'img/cthulhu.jpg');
-new Item('Dog-duck', 'img/dog-duck.jpg');
-new Item('Dragon', 'img/dragon.jpg');
-new Item('Pen', 'img/pen.jpg');
-new Item('Pet-sweep', 'img/pet-sweep.jpg');
-new Item('Tauntaun', 'img/tauntaun.jpg');
-new Item('Unicorn', 'img/unicorn.jpg');
-new Item('USB', 'img/usb.gif');
-new Item('Water-can', 'img/water-can.jpg');
-new Item('Wine-glass', 'img/Wine-glass.jpg');
-
-var imgRandom = function () {
-  return Math.floor(Math.random() * allItems.length);
-};
-
-//function that makes images appear
-function itemRender(){
-
-  if (globalClicks === 25) {
-    removeDummy();
+function renderItems() {
+  var showing = [];
+  showing[0] = makeRandom();
+  while (Item.viewed.indexOf(showing[0]) !== -1) {
+    showing[0] = makeRandom();
+  }
+  showing[1] = makeRandom();
+  while(showing[0] === showing[1] || Item.viewed.indexOf(showing[1]) !== -1) {
+    showing[1] = makeRandom();
+  }
+  showing[2] = makeRandom();
+  while(showing[0] === showing[2] || showing[1] === showing[2] || Item.viewed.indexOf(showing[2]) !== -1) {
+    showing[2] = makeRandom();
   }
 
-  /* ******************************************************
-  need to add while loop for each image and just run a random func for that image instead of running the whole itemRender.
-     ********************************************************* */
-    
-  img1 = imgRandom();
-  img2 = imgRandom();
-  img3 = imgRandom();
-  itemPic1.src = allItems[img1].filePath;
-  itemPic2.src = allItems[img2].filePath;
-  itemPic3.src = allItems[img3].filePath;
-  // allItems[img1].timesDisplayed ++;
-  // allItems[img2].timesDisplayed ++;
-  // allItems[img3].timesDisplayed ++;
-  console.log('before function ' + itemDisplayed);
-  itemDisplayed.push(img1, img2, img3);
-  while (itemDisplayed[0] === img1 || itemDisplayed[1] === img2 || itemDisplayed[2] === img3 || itemDisplayed[0] === img3 || img1 === img2 || img2 === img3 || img1 === img3){
-    itemRender();
+  for(var i = 0; i < 3; i++) {
+    Item.pics[i].src = Item.all[showing[i]].path;
+    Item.pics[i].id = Item.all[showing[i]].name;
+    Item.all[showing[i]].views += 1;
+    Item.viewed[i] = showing[i];
   }
-  console.log('outside function ' + itemDisplayed);
-}
-itemRender();
-
-function handleClick(image){
-  itemDisplayed = [];
-  image.clickTotal++;
-  globalClicks++;
-  console.log('total clicks ' + globalClicks);
-  console.log(image.productName + ' has ' + image.clickTotal + ' total votes.');
-  itemRender();
 }
 
-
-itemPic1.addEventListener('click', function(){
-  handleClick(allItems[img1]);
-});
-itemPic2.addEventListener('click', function(){
-  handleClick(allItems[img2]);
-});
-itemPic3.addEventListener('click', function(){
-  handleClick(allItems[img3]);
-});
-
-
-function removeDummy() {
-  var container = document.getElementById('container');
-  container.removeChild(itemPic1);
-  container.removeChild(itemPic2);
-  container.removeChild(itemPic3);
+function handleClick(event) {
+  if(Item.totalClicks > 24) {
+    Item.container.removeEventListener('click', handleClick);
+    showtotals();
+  }
+  if (event.target.id === 'container') {
+    return alert('Please make a selection');
+  }
+  Item.totalClicks += 1;
+  for( var i = 0; i < Item.names.length; i++) {
+    if(event.target.id === Item.all[i].name) {
+      Item.all[i].votes += 1;
+    }
+  }
+  renderItems();
 }
+
+function showtotals() {
+  for(var i = 0; i < Item.all.length; i++) {
+    var liEl = document.createElement('li');
+    liEl.textContent = ' ' + Item.all[i].name + ' has ' + Item.all[i].votes + ' votes in ' + Item.all[i].views + ' views. ';
+    Item.totals.appendChild(liEl);
+  }
+}
+
+Item.container.addEventListener('click', handleClick);
+renderItems();
